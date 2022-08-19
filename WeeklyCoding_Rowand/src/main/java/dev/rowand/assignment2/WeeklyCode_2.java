528b337d06f1d7b87787d998165de3a21a842672
Trowand <trowand34@gmail.com>
2022-08-19 00:29:13 -0500

    completed assignment2 now

--- WeeklyCoding_Rowand/src/main/java/dev/rowand/assignment2/WeeklyCode_2.java
+++ WeeklyCoding_Rowand/src/main/java/dev/rowand/assignment2/WeeklyCode_2.java
@@ -14,36 +14,79 @@ public class WeeklyCode_2 {
 		System.out.println("number of test cases: ");
 		 int testCase = scan.nextInt();
 		 
-		 String[] Mnames = new String[testCase];
+		 String[] Mnames = new String[testCase]; //storage for male names
 		 
-		 String[] Wnames = new String[testCase];
+		 String[] Wnames = new String[testCase]; //storage for woman names
 		 
 		 
-		 for(int x = 0; x < Mnames.length; x++) {
+		 for(int x = 0; x < testCase; x++) {
 			 String input = scan.next();
-			 String[] stor = input.split(" ");
+			 String[] stor = input.split(","); //couldn't get split with " " working, so used "," instead
+			
 			 Mnames[x] = stor[0];
 			 Wnames[x] = stor[1];
 		 }
-		 for(int x = 0; x < Mnames.length; x++) {
-			 List<Character> chars = new ArrayList<>();
-			 if(Mnames[x].length() == Wnames[x].length()) {
+		 for(int y = 0; y < testCase; y++) {
+
+			 List<Character> chars = new ArrayList<>(); //list that will be compared with the smaller name
+			 if(Mnames[y].length() > Wnames[y].length()) { //if Mname is better then Wname
+				 for(int a = 0; a > Mnames[y].length(); a++) {
+					 String hold = String.valueOf(Mnames[y].charAt(a)); //convert char to string
+					 if(Wnames[y].contains(hold)) { //for comparison to work
+						 String mn = chars.toString(); //make string out of char list
+						 String check = String.valueOf(Mnames[y].charAt(a)); //convert curr char into string for contains
+						 if(!mn.contains(check)) { //if mn doesn't already have check in it
+							 chars.add(Mnames[y].charAt(a)); //add check to the chars list
+						 }
+						 
+					 }
+				 }
+				 StringBuilder charcon = new StringBuilder(); //stores converted format from [a, b, c, d, etc.] to abcd
+				 for(int l = 0; l < chars.size(); l++) {
+					 charcon.append(chars.get(l));
+				 }
+				 if(Wnames[y].contains(charcon)) { //if current Wname equals charcon
+					 
+					 System.out.println("YES"); //we have a match!
+				 }
+				 else {
+					 System.out.println("NO"); //we don't have a match in the pattern
+				 }
+				 
 				 
 			 }
-			 else {
-				 for(int y = 0; y < Wnames[x].length(); y++) {
-					 if(chars.size() == Mnames[x].length()) {
-						 break;
-					 }
-					 else {
-						 CharSequence check = Wnames[x].subSequence(y, y);
-						 if(Mnames[x].contains(check)) {
-							 chars.add()
+			 else if (Mnames[y].length() < Wnames[y].length()) { //if Wname is bigger then Mname
+				 for(int a = 0; a < Wnames[y].length(); a++) {
+					 String hold = String.valueOf(Wnames[y].charAt(a));
+					 if(Mnames[y].contains(hold)) {
+						 String mn = chars.toString();
+						 String check = String.valueOf(Wnames[y].charAt(a));
+						 if(!mn.contains(check)) {
+						 chars.add(Wnames[y].charAt(a));
 						 }
 					 }
 				 }
+				 StringBuilder charcon = new StringBuilder();
+				 for(int l = 0; l < chars.size(); l++) {
+					 charcon.append(chars.get(l));
+				 }
+				 if(Mnames[y].contains(charcon)) {
+					 System.out.println("YES");
+				 }
+				 else {
+					 System.out.println("NO");
+				 }
+			 }
+			 else {
+				 if(Wnames[y].equals(Mnames[y])) {
+					 System.out.println("YES");
+				 }
+				 else {
+					 System.out.println("NO");
+				 }
 			 }
 		 }
+
 		 
 
 	}
