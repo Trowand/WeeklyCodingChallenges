ca237ebc8b7358d6c68a82e559436a4c5f9f03ee
Trowand <trowand34@gmail.com>
2022-09-21 13:38:17 -0500

    second problem for assignment 6 done!

--- WeeklyCoding_Rowand/src/main/java/dev/rowand/assignment6/StringDigitSum.java
+++ WeeklyCoding_Rowand/src/main/java/dev/rowand/assignment6/StringDigitSum.java
@@ -1,10 +1,26 @@
 package dev.rowand.assignment6;
 
+import java.util.Scanner;
+
 public class StringDigitSum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+		Scanner scan = new Scanner(System.in);
+		int count = scan.nextInt();
+		String[] holder = new String[count];
+		for(int x = 0; x < count; x++) {
+			holder[x] = scan.next();
+		}
+		for(int y = 0; y < count; y++) {
+			int digits = 0;
+			for(int z = 0; z < holder[y].length(); z++) {
+				if(holder[y].charAt(z) >= 49 && holder[y].charAt(z) <= 57) {
+					digits = digits + Character.getNumericValue(holder[y].charAt(z));
+				}
+			}
+			System.out.println(digits);
+		}
+;	}
 
 }
